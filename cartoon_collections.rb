afcf43e32c1db8876f39d94a9f4523045c60f33e
Darrow Palast <dpalast@gmail.com>
2019-08-03 18:16:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarf, i|
-    puts "#{i+1}. #{dwarf}"
-  end
+  dwarves.each_with_index { |dwarf, i| puts "#{i+1}. #{dwarf}" }
 end
 
 def summon_captain_planet(calls)
